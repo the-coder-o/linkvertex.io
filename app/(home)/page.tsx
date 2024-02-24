3c0e83bf88523e51e5f1c9a683ad951d6a038a5f
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-02-24 12:01:13 +0500

    started home page

---
+++ app/(home)/page.tsx
@@ -0,0 +1,25 @@
+export default function Home() {
+  return (
+    <>
+      <main className={"container"}>
+        <div
+          className={
+            "py-[12rem] flex items-center flex-col justify-center gap-[3.5rem]"
+          }
+        >
+          <h1
+            className={
+              "main-text-animation w-[410px] text-[3.80rem] text-center leading-[110%] font-[800]"
+            }
+          >
+            One Place for All Your Links
+          </h1>
+          <p className={"w-[410px] font-[600] text-white leading-[1.5rem] text-center"}>
+            One link to rule them all. Easily place your links in a beautiful
+            single page.
+          </p>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ components/ui/header/header.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { Sun } from "lucide-react";
+
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className={"container"}>
+      <div className={"pt-5 flex items-center justify-between"}>
+        <div>
+          <h1 className={"text-white text-[1.25rem] font-bold"}>
+            <Link href={"/"}>Linkvertex.io</Link>
+          </h1>
+        </div>
+        <div>
+          <Sun className={'text-white w-[1rem] h-[1rem]'} />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
