6a2fa9982f56bed8ccec25055d6b2794fcf61079
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-02-25 14:17:28 +0500

    test request

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 
+import Link from 'next/link'
+import Image from 'next/image'
+
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
 import GoogleIcons from '@/assets/auth/google.png'
 import GithubIcons from '@/assets/auth/github.png'
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-import { Input } from '@/components/ui/input'
-import Link from 'next/link'
+import { Eye } from 'lucide-react'
 
 const SignUp = () => {
   return (
     <div className={'container'}>
-      <div className={'flex flex-col items-center justify-center gap-[20px] my-[160px]'}>
+      <div className={'flex flex-col items-center justify-center gap-[20px] my-[130px] max-sm:my-[90px] max-sm:gap-[10px]'}>
         <div className={'flex items-center flex-col justify-center gap-2.5'}>
           <p className={'text-white text-[0.875rem]'}>Sign Up with:</p>
-          <div className={'flex items-center justify-between w-[440px] gap-2'}>
+          <div className={'flex items-center justify-between w-[440px] max-sm:w-full gap-2'}>
             <Button className={'flex items-center gap-2 rounded-[20px] px-[16px] text-[16px] font-[500] bg-transparent border border-[#ffffff29] hover:bg-[#C8E7F7] hover:text-black w-full'}>
               <Image width={13} height={13} src={GoogleIcons} alt={'GoogleIcons'} />
               Google
@@ -25,10 +28,18 @@ const SignUp = () => {
           </div>
         </div>
         <p className={'text-[#464c4b] text-center'}>or</p>
-        <div className={'w-[440px]'}>
+        <div className={'w-[440px] max-sm:w-full'}>
           <form className={'flex flex-col items-center justify-center gap-[1.5rem]'}>
             <Input type={'email'} placeholder={'email'} className={'rounded-[20px] px-[16px] text-white bg-transparent border-2 border-[#feb2b2] text-[16px] !outline-none'} />
-            <Input type={'password'} placeholder={'password'} className={'rounded-[20px] px-[16px] text-white bg-transparent border-2 border-[#feb2b2] text-[16px] !outline-none'} />
+            <div className={'w-full'}>
+              <Input type={'password'} placeholder={'password'} className={'rounded-[20px] px-[16px] text-white bg-transparent border-2 border-[#feb2b2] text-[16px] !outline-none'} />
+            </div>
+            <div className={'w-full relative'}>
+              <Input type={'password'} placeholder={'repeat password'} className={'rounded-[20px] px-[16px] text-white bg-transparent border-2 border-[#feb2b2] text-[16px] !outline-none'} />
+              <div className={'bg-white/10 rounded-full absolute top-0 right-0 flex items-center justify-center w-[40px] h-[40px]'}>
+                <Eye className={'text-white w-5 h-5 flex items-center justify-center'} />
+              </div>
+            </div>
             <Button className={'flex items-center gap-2 rounded-[20px] px-[16px] text-[16px] font-[500] bg-[#81E6D9] hover:bg-[#81E6D9]/90 text-black w-full'}>Sign Up</Button>
             <Link href={'/'} className={'text-[16px] text-white font-[600] hover:underline'}>
               Forgot Password?
