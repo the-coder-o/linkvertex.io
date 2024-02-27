e2ed690ecfc95c56be86840519a0e9b767c40aef
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-02-27 20:29:04 +0500

    test request

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -44,21 +44,23 @@ const Profile = () => {
     setIsValid({ ...isValid, [service]: validator(value) })
   }
 
+  const isSaveDisabled = Object.values(isValid).some((value) => !value)
+
   return (
     <div className={'container'}>
       <div className={'mt-[100px]'}>
         <div>
-          <Link href={'/dashboard'} className={'bg-[#1D1F1F]/80 rounded-[24px] text-white flex justify-center font-[500] pr-[10px] w-[13%] items-center py-[8px] text-[16px] hover:bg-[#1D1F1F] transition-all'}>
+          <Link href={'/dashboard'} className={'bg-[#1D1F1F]/80 rounded-[24px] text-white flex justify-center font-[500] pr-[10px] w-[13%] items-center py-[8px] text-[16px] hover:bg-[#1D1F1F] transition-all max-sm:w-[100px]'}>
             <ChevronLeft className={'w-5 h-5 font-bold'} />
             back
           </Link>
         </div>
         <div className={'mb-[3rem]'}>
-          <h2 className={'text-4xl main-text-animation font-bold my-[16px]'}>Profile Settings</h2>
+          <h2 className={'text-4xl main-text-animation font-bold my-[16px] max-sm:text-[30px]'}>Profile Settings</h2>
         </div>
         <div className={'flex flex-col gap-[2.5rem]'}>
           <div>
-            <h2 className={'text-3xl font-bold text-white mb-[16px]'}>Social Links</h2>
+            <h2 className={'text-3xl font-bold text-white mb-[16px] max-sm:text-[26px]'}>Social Links</h2>
           </div>
           <div className={'grid grid-cols-1 space-y-[2rem] mb-[100px]'}>
             <div className={'flex flex-row gap-[1rem]'}>
@@ -308,7 +310,9 @@ const Profile = () => {
                 </div>
               </div>
             </div>
-            <Button className={'bg-[#9ae6b4] rounded-[20px] font-semibold text-black text-md hover:bg-[#9ae6c9]'}>Save</Button>
+            <Button disabled={isSaveDisabled} className={'bg-[#9ae6b4] rounded-[20px] font-semibold text-black text-md hover:bg-[#9ae6c9]'}>
+              Save
+            </Button>
           </div>
         </div>
       </div>
