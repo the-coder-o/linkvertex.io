d027bccd90cacf9c9779981893a7c889a93ab496
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-02-29 22:34:48 +0500

    test

--- app/dashboard/edit/page.tsx
+++ app/dashboard/edit/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
-import { ChevronLeft, Link2, ShieldAlert, Tags } from 'lucide-react'
+import { ChevronLeft, Link2, ShieldAlert, Tags, Trash } from 'lucide-react'
 
 import { Input } from '@/components/ui/input'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
@@ -12,7 +12,21 @@ import Link from 'next/link'
 
 const regexForPageTitle: RegExp = /^[A-Za-z-]+$/
 
-const Edit = () => {
+interface LinkItem {
+  id: string
+  title: string // Added title property
+  url: string
+  enabled: boolean
+}
+
+interface LinkItemProps {
+  link: LinkItem
+  onToggle: (id: string) => void
+  onEdit: (id: string, title: string, url: string) => void
+  onDelete: (id: string) => void
+}
+
+const Edit = ({ url, id, enabled, title }: LinkItem, { onEdit, onDelete, link, onToggle }: LinkItemProps) => {
   const [inputValue, setInputValue] = useState('')
   const [isValid, setIsValid] = useState(true)
 
@@ -23,9 +37,29 @@ const Edit = () => {
     setIsValid(regexForPageTitle.test(value))
   }
 
+  const [links, setLinks] = useState<LinkItem[]>([])
+
+  const addLink = () => {
+    const newLink: LinkItem = {
+      id: Date.now().toString(),
+      title: '',
+      url: '',
+      enabled: true,
+    }
+    setLinks((prevLinks) => [...prevLinks, newLink])
+  }
+
+  const toggleLink = useCallback((id: string) => {
+    setLinks((prevLinks) => prevLinks.map((link) => (link.id === id ? { ...link, enabled: !link.enabled } : link)))
+  }, [])
+
+  const deleteLink = useCallback((id: string) => {
+    setLinks((prevLinks) => prevLinks.filter((link) => link.id !== id))
+  }, [])
+
   return (
     <div className={'container'}>
-      <div className={'mt-[100px]'}>
+      <div className={'mt-[100px] mb-[100px]'}>
         <div>
           <Link href={'/dashboard'} className={'bg-[#1D1F1F]/80 rounded-[24px] text-white flex justify-center font-[500] pr-[10px] w-[13%] items-center py-[8px] text-[16px] hover:bg-[#1D1F1F] transition-all max-sm:w-[100px]'}>
             <ChevronLeft className={'w-5 h-5 font-bold'} />
@@ -68,34 +102,42 @@ const Edit = () => {
               <AlertDescription className={'text-white ml-2 text-[16px] font-medium'}>To edit social links, go to Profile Settings. You can also use drag and drop to arrange the links sequence.</AlertDescription>
             </Alert>
             <div className={'border-2 border-[#ffffff29] rounded-[24px] p-[0.5rem]'}>
-              <div className={'grid grid-cols-1 border-1 border-[#ffffff29] bg-[#1a202c99] p-[1rem] rounded-[24px] mb-[15px]'}>
-                <h2 className={'text-md uppercase font-bold text-white'}>URL 1</h2>
-                <div className={'relative'}>
-                  <Input
-                    placeholder={'label'}
-                    type={'text'}
-                    className={'mt-[0.5rem] !bg-[#1C2129] border-transparent focus:!bg-transparent rounded-[20px] px-[40px] bg-transparent text-[1rem] pt-[5px] text-white placeholder:text-[#454646] placeholder:font-medium focus:!border-2 focus:!transition focus:!border-[#63b3ed]'}
-                  />
-                  <Tags className={'text-white absolute top-[18px] left-3 w-5 h-5'} />
-                </div>
-                <div className={'relative'}>
-                  <Input
-                    placeholder={'link url'}
-                    type={'text'}
-                    className={'mt-[0.5rem] !bg-[#1C2129] border-transparent focus:!bg-transparent rounded-[20px] px-[40px] bg-transparent text-[1rem] pt-[5px] text-white placeholder:text-[#454646] placeholder:font-medium focus:!border-2 focus:!transition focus:!border-[#63b3ed]'}
-                  />
-                  <Link2 className={'text-white absolute top-[18px] left-3 w-5 h-5'} />
-                </div>
-                <div>
-                  <div className="flex items-center space-x-2 mt-[15px]">
+              {links?.map((link: any, index: number) => (
+                <div className={'grid grid-cols-1 border-1 border-[#ffffff29] bg-[#1a202c99] p-[1rem] rounded-[24px] mb-[15px]'}>
+                  <h2 className={'text-md uppercase font-bold text-white'}>URL {index}</h2>
+                  <div className={'relative'}>
+                    <Input
+                      placeholder={'label'}
+                      value={link.title}
+                      onChange={(e) => onEdit(link.id, e.target.value, link.url)}
+                      type={'text'}
+                      className={'mt-[0.5rem] !bg-[#1C2129] border-transparent focus:!bg-transparent rounded-[20px] px-[40px] bg-transparent text-[1rem] pt-[5px] text-white placeholder:text-[#454646] placeholder:font-medium focus:!border-2 focus:!transition focus:!border-[#63b3ed]'}
+                    />
+                    <Tags className={'text-white absolute top-[18px] left-3 w-5 h-5'} />
+                  </div>
+                  <div className={'relative'}>
+                    <Input
+                      placeholder={'link url'}
+                      value={link?.url}
+                      onChange={(e) => onEdit(link?.id, link?.title, e.target?.value)}
+                      type={'text'}
+                      className={'mt-[0.5rem] !bg-[#1C2129] border-transparent focus:!bg-transparent rounded-[20px] px-[40px] bg-transparent text-[1rem] pt-[5px] text-white placeholder:text-[#454646] placeholder:font-medium focus:!border-2 focus:!transition focus:!border-[#63b3ed]'}
+                    />
+                    <Link2 className={'text-white absolute top-[18px] left-3 w-5 h-5'} />
+                  </div>
+                  <div className={'flex items-center justify-between mt-[15px]'}>
                     <Switch id="airplane-mode" />
+                    <Trash className={'text-red-500 w-5 h-5 cursor-pointer'} />
                   </div>
                 </div>
-              </div>
+              ))}
               <div>
-                <Button className={'justify-center py-2 !font-bold flex items-center gap-2 rounded-[20px] px-[16px] text-[16px] bg-[#90CDF4] hover:bg-[#90CDF4]/90 text-black w-full'}>Add Link</Button>
+                <Button onClick={addLink} className={'justify-center py-2 !font-bold flex items-center gap-2 rounded-[20px] px-[16px] text-[16px] bg-[#90CDF4] hover:bg-[#90CDF4]/90 text-black w-full'}>
+                  Add Link
+                </Button>
               </div>
             </div>
+            <Button className={'justify-center py-6 !font-bold flex items-center gap-2 rounded-[18px] px-[16px] text-[16px] bg-[#9AE6B4] hover:bg-[#90CDF4]/90 text-black w-full'}>Save</Button>
           </div>
           <div></div>
         </div>
