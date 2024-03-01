8c39cc44f8b941b8533fe93552b373dee2f6e884
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-03-01 19:37:50 +0500

    fixed a lot errors and finished edit page

--- app/dashboard/edit/page.tsx
+++ app/dashboard/edit/page.tsx
@@ -8,6 +8,7 @@ import { Input } from '@/components/ui/input'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { Switch } from '@/components/ui/switch'
 import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
 
 const regexForPageTitle = /^[A-Za-z-]+$/
 
@@ -30,6 +31,7 @@ const EditPage: React.FC = () => {
       enabled: true,
     }
     setLinks((prevLinks) => [...prevLinks, newLink])
+    console.log(newLink)
   }, [])
 
   const toggleLinkEnabled = useCallback((id: string) => {
@@ -42,6 +44,7 @@ const EditPage: React.FC = () => {
 
   const handleLinkEdit = useCallback((id: string, title: string, url: string) => {
     setLinks((prevLinks) => prevLinks.map((link) => (link.id === id ? { ...link, title, url } : link)))
+    console.log(links)
   }, [])
 
   return (
@@ -55,25 +58,25 @@ const EditPage: React.FC = () => {
         <div className="mb-[3rem]">
           <h2 className="text-4xl main-text-animation font-bold my-[16px] max-sm:text-[30px]">Edit Links</h2>
         </div>
-        <div>
+        <div className={'flex justify-between gap-[30px]'}>
           <div className="grid grid-cols-1 gap-y-12">
             <div className={'grid grid-cols-1 gap-y-12'}>
               <div>
-                <label className={'text-start text-white text-md font-medium'}>title</label>{' '}
+                <label className={'text-start text-white text-md font-medium'}>title</label>
                 <Input
                   type={'text'}
                   className={'mt-[0.5rem] !bg-[#141616] border-transparent focus:!bg-transparent rounded-[20px] px-[16px] bg-transparent text-[1rem] pt-[5px] text-white placeholder:text-[#454646] placeholder:font-medium focus:!border-2 focus:!transition focus:!border-[#63b3ed]'}
-                />{' '}
+                />
               </div>
               <div>
-                <label className={'text-start text-white text-md font-medium'}>description</label>{' '}
+                <label className={'text-start text-white text-md font-medium'}>description</label>
                 <Input
                   type={'text'}
                   className={'mt-[0.5rem] !bg-[#141616] border-transparent focus:!bg-transparent rounded-[20px] px-[16px] bg-transparent text-[1rem] pt-[5px] text-white placeholder:text-[#454646] placeholder:font-medium focus:!border-2 focus:!transition focus:!border-[#63b3ed]'}
-                />{' '}
+                />
               </div>
               <div>
-                <label className={'text-start text-white text-md font-medium'}>page title</label>{' '}
+                <label className={'text-start text-white text-md font-medium'}>page title</label>
                 <div className="relative">
                   <Input
                     value={inputValue}
@@ -83,23 +86,21 @@ const EditPage: React.FC = () => {
                     id="hs-inline-add-on"
                     name="hs-inline-add-on"
                   />
-                  <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
+                  <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-4">
                     <span className="text-start text-gray-400 text-sm font-medium">https://linkvertex-a-bd.me/</span>{' '}
                   </div>
                 </div>
                 {!isValid && <p className="text-red-500 text-sm">Please write a correct your page url title.</p>}{' '}
               </div>
             </div>
-
             <Alert className="!bg-[#1E2B32] border-transparent rounded-[24px]">
               <ShieldAlert className="bg-[#90CDF4] rounded-full flex items-center justify-center p-[4px]" />
               <AlertTitle className="text-white ml-2">Remember!</AlertTitle>
               <AlertDescription className="text-white ml-2 text-[16px] font-medium">To edit social links, go to Profile Settings. You can also use drag and drop to arrange the links sequence.</AlertDescription>
             </Alert>
-
-            <div className="border-2 border-[#ffffff29] rounded-[24px] p-[0.5rem]">
+            <div className="border-2 border-[#ffffff29] grid grid-cols-1 items-center rounded-[24px] p-[0.5rem]">
               {links.map((link, index) => (
-                <div key={link.id} className="grid grid-cols-1 border-1 border-[#ffffff29] bg-[#1a202c99] p-[1rem] rounded-[24px] mb-[15px]">
+                <div key={link.id} className={cn(link ? 'grid grid-cols-1 border-1 border-[#ffffff29] bg-[#1a202c99] p-[1rem] rounded-[24px] mb-[15px]' : 'hidden')}>
                   <h2 className="text-md uppercase font-bold text-white">URL {index + 1}</h2>
                   <div className="relative">
                     <Input
@@ -127,12 +128,28 @@ const EditPage: React.FC = () => {
                   </div>
                 </div>
               ))}
-              <Button onClick={addLink} className="justify-center py-2 !font-bold flex items-center gap-2 rounded-[20px] px-[16px] text-[16px] bg-[#90CDF4] hover:bg-[#90CDF4]/90 text-black w-full">
+              <Button onClick={addLink} className="!font-bold flex items-center justify-center rounded-[20px] px-[16px] text-[16px] bg-[#90CDF4] hover:bg-[#90CDF4]/90 text-black w-full">
                 Add Link
               </Button>
             </div>
-
             <Button className="justify-center py-6 !font-bold flex items-center gap-2 rounded-[18px] px-[16px] text-[16px] bg-[#9AE6B4] hover:bg-[#9AE6B4]/90 text-black w-full">Save</Button>
+          </div>
+          <div>
+            <div className={'p-[1rem] border-8 rounded-[32px] border-[#ffffff29] sticky top-[10%]'}>
+              <div className={'w-[300px] h-[680px] flex items-center justify-center'}>
+                <div className={'grid grid-cols-1 gap-y-4'}>
+                  <div className={'flex items-center justify-center flex-col'}>
+                    <h1 className={'text-4xl font-bold text-white'}>Abdul Basit</h1>
+                    <p className={'text-white text-sm w-full text-center'}>Im looking for sponsorships for the featured repos below. This sponsorship will go towards maintaining some open-source applications with ongoing development.</p>
+                  </div>
+                  {links?.map((links: LinkItem) => (
+                    <Link href={links?.url} className={'flex items-center justify-center text-white font-semibold text-lg border-2 border-[#ffffff29] bg-[#0e1312] leading-[1.2] w-[300px] h-[3rem] p-[0.75rem] rounded-[20px] hover:scale-105 transition-all shadow-style'}>
+                      {links?.title}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
