a18d1915272fed7998fe5df7e0352b43894af622
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-03-04 12:06:54 +0500

    realazed new page analytics for dahsboard for v2.0.0

---
+++ app/dashboard/analytics/page.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
+import Footer from '@/components/ui/footer/footer'
+import CountdownTimer from '@/components/soon/count-down-time'
+
+const Page = () => {
+  const launchDate = new Date('2024-11-11T00:00:00')
+
+  return (
+    <div className={'container'}>
+      <div className={'flex items-center justify-center flex-col mt-[250px] gap-[15px]'}>
+        <motion.h1 initial={{ y: 20, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.3, delay: 0.1 }} className={'text-white text-[55px] w-[500px] text-center font-bold leading-tight'}>
+          This <span className={'text-red-400'}>Analytic</span> page Coming Soon
+        </motion.h1>
+        <CountdownTimer targetDate={launchDate} />
+        <motion.p initial={{ y: 20, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.3, delay: 3 * 0.1 }} className={'text-white w-[70%] text-center'}>
+          We are excited to announce that we will be launching soon and can't wait to share our new platform with you.
+        </motion.p>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Page
