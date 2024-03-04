ed61ac5bf82273c4bcb33ba9859b6348050b1849
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-03-04 11:29:37 +0500

    chnged some errors

--- components/ui/header/header.tsx
+++ components/ui/header/header.tsx
@@ -45,9 +45,9 @@ const Header = () => {
             </h1>
           </div>
           <div className={'flex items-center gap-3'}>
-            <nav>
+            <nav className={'flex items-center gap-10'}>
               {UrlPages?.map((item: { linkTitle: string; linkPath: string }, index) => (
-                <Link key={index} href={item.linkPath} className={cn('hover:text-white/80 text-[#b4b4b4] transition-all text-[16px] font-bold px-4 py-1.5 rounded-lg', item.linkPath === pathName && 'transition-all text-white')}>
+                <Link key={index} href={item.linkPath} className={cn('hover:text-white/80 text-[#b4b4b4] transition-all text-[16px] pt-1.5 font-bold rounded-lg', item.linkPath === pathName && 'transition-all text-white')}>
                   {item.linkTitle}
                 </Link>
               ))}
