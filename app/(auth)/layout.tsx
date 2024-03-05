f9e9425691607f18a383d0fd9a5ca8e2b7387a9d
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-03-05 20:35:36 +0500

    fixed some styles and errors

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Footer from '@/components/ui/footer/footer'
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className={'h-full flex items-center justify-center mt-[150px]'}>{children}</div>
+  return (
+    <div className={'h-full flex items-center justify-center mt-[150px]'}>
+      {children}
+      <Footer />
+    </div>
+  )
 }
 
 export default AuthLayout
