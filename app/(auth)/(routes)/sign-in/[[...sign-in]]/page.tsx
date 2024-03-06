323da5a4c42c05d73d2ad45bed4f37ff586a4e83
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-03-06 16:16:18 +0500

    finished some styles and responsive design

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div className={'mt-[130px]'}>
+    <div className={'mt-[130px] max-sm:mt-[100px]'}>
       <SignIn />
     </div>
   )
--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div className={'mt-[120px]'}>
+    <div className={'mt-[120px] max-sm:mt-[100px]'}>
       <SignUp />
     </div>
   )
