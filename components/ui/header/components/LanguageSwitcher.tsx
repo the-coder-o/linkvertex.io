e6ce414284d995be5643b4e628e63433753d0fca
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-03-08 14:08:57 +0500

    fixed errors from localization.

--- components/ui/header/components/LanguageSwitcher.tsx
+++ components/ui/header/components/LanguageSwitcher.tsx
@@ -21,30 +21,27 @@ interface LanguageProviderProps {
 }
 
 const LanguageProvider: React.FC<LanguageProviderProps> = ({ children, languages }) => {
-  // Initially, we don't have a selected language until we've checked local storage
   const [selectedLanguage, setSelectedLanguage] = useState<Language | null>(null)
   const { i18n } = useTranslation()
 
   useEffect(() => {
-    // Function to fetch the stored language or default to the first one
     const getDefaultLanguage = (): Language => {
-      if (typeof window === 'undefined') return languages[0] // SSR
+      if (typeof window === 'undefined') return languages[0]
 
       const storedLanguageCode = localStorage.getItem('selectedLanguageCode')
       if (storedLanguageCode) {
         const storedLanguage = languages.find((lang) => lang.code === storedLanguageCode)
         if (storedLanguage) return storedLanguage
       }
-      return languages[0] // Default language if not found in storage
+      return languages[0]
     }
 
     const defaultLanguage = getDefaultLanguage()
-    setSelectedLanguage(defaultLanguage) // Set the language from storage or default
-    i18n.changeLanguage(defaultLanguage.code) // Set i18n language as well
+    setSelectedLanguage(defaultLanguage)
+    i18n.changeLanguage(defaultLanguage.code)
   }, [languages, i18n])
 
   useEffect(() => {
-    // Don't try to set local storage during SSR
     if (selectedLanguage && typeof window !== 'undefined') {
       localStorage.setItem('selectedLanguageCode', selectedLanguage.code)
       i18n.changeLanguage(selectedLanguage.code)
@@ -57,7 +54,6 @@ const LanguageProvider: React.FC<LanguageProviderProps> = ({ children, languages
     setSelectedLanguage(languages[nextIndex])
   }
 
-  // Render nothing until we have a selected language
   if (!selectedLanguage) return null
 
   return <LanguageContext.Provider value={{ handleLanguageChange, selectedLanguage }}>{children}</LanguageContext.Provider>
