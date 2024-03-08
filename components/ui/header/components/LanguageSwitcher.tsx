3d468612bcaba890694884845ae5acb29116c900
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-03-08 13:59:50 +0500

    started translate for all pages eng,rus,ger,uzbd and optimized all header codes

--- components/ui/header/components/LanguageSwitcher.tsx
+++ components/ui/header/components/LanguageSwitcher.tsx
@@ -21,30 +21,44 @@ interface LanguageProviderProps {
 }
 
 const LanguageProvider: React.FC<LanguageProviderProps> = ({ children, languages }) => {
-  const getDefaultLanguage = () => {
-    const storedLanguageCode = localStorage.getItem('selectedLanguageCode')
-    if (storedLanguageCode) {
-      const storedLanguage = languages.find((lang) => lang.code === storedLanguageCode)
-      if (storedLanguage) {
-        return storedLanguage
-      }
-    }
-    return languages[0]
-  }
-
-  const [selectedLanguage, setSelectedLanguage] = useState<Language>(getDefaultLanguage())
+  // Initially, we don't have a selected language until we've checked local storage
+  const [selectedLanguage, setSelectedLanguage] = useState<Language | null>(null)
   const { i18n } = useTranslation()
 
   useEffect(() => {
-    localStorage.setItem('selectedLanguageCode', selectedLanguage.code)
-    i18n.changeLanguage(selectedLanguage.code)
+    // Function to fetch the stored language or default to the first one
+    const getDefaultLanguage = (): Language => {
+      if (typeof window === 'undefined') return languages[0] // SSR
+
+      const storedLanguageCode = localStorage.getItem('selectedLanguageCode')
+      if (storedLanguageCode) {
+        const storedLanguage = languages.find((lang) => lang.code === storedLanguageCode)
+        if (storedLanguage) return storedLanguage
+      }
+      return languages[0] // Default language if not found in storage
+    }
+
+    const defaultLanguage = getDefaultLanguage()
+    setSelectedLanguage(defaultLanguage) // Set the language from storage or default
+    i18n.changeLanguage(defaultLanguage.code) // Set i18n language as well
+  }, [languages, i18n])
+
+  useEffect(() => {
+    // Don't try to set local storage during SSR
+    if (selectedLanguage && typeof window !== 'undefined') {
+      localStorage.setItem('selectedLanguageCode', selectedLanguage.code)
+      i18n.changeLanguage(selectedLanguage.code)
+    }
   }, [selectedLanguage, i18n])
 
   const handleLanguageChange = () => {
-    const currentIndex = languages.findIndex((lang) => lang.code === selectedLanguage.code)
+    const currentIndex = languages.findIndex((lang) => lang === selectedLanguage)
     const nextIndex = (currentIndex + 1) % languages.length
     setSelectedLanguage(languages[nextIndex])
   }
+
+  // Render nothing until we have a selected language
+  if (!selectedLanguage) return null
 
   return <LanguageContext.Provider value={{ handleLanguageChange, selectedLanguage }}>{children}</LanguageContext.Provider>
 }
