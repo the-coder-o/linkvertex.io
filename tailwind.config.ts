4e9f06a9a70c4086e38d8e28afd29287c8468426
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-03-09 19:21:20 +0500

    fixed all bugs from clerk auth and chnaged some styles from about page, optimized all codes.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,21 +12,6 @@ const config = {
   theme: {
     container: { center: true, padding: '2rem', screens: { '2xl': '1400px' } },
     extend: {
-      animation: {
-        spotlight: 'spotlight 2s ease .75s 1 forwards',
-      },
-      keyframes: {
-        spotlight: {
-          '0%': {
-            opacity: 0,
-            transform: 'translate(-72%, -62%) scale(0.5)',
-          },
-          '100%': {
-            opacity: 1,
-            transform: 'translate(-50%,-40%) scale(1)',
-          },
-        },
-      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
@@ -42,6 +27,8 @@ const config = {
         card: { DEFAULT: 'hsl(var(--card))', foreground: 'hsl(var(--card-foreground))' },
       },
       borderRadius: { lg: 'var(--radius)', md: 'calc(var(--radius) - 2px)', sm: 'calc(var(--radius) - 4px)' },
+      keyframes: { 'accordion-down': { from: { height: '0' }, to: { height: 'var(--radix-accordion-content-height)' } }, 'accordion-up': { from: { height: 'var(--radix-accordion-content-height)' }, to: { height: '0' } } },
+      animation: { 'accordion-down': 'accordion-down 0.2s ease-out', 'accordion-up': 'accordion-up 0.2s ease-out' },
     },
   },
   plugins: [
