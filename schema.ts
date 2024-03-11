8e3c50e62ea7f7301323c81e25e0bf463234edaa
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-03-12 00:07:02 +0500

    test

---
+++ schema.ts
@@ -0,0 +1,26 @@
+const schema = [
+  {
+    email: 'string',
+    username: 'string',
+    password: 'string',
+  },
+]
+
+const account = [
+  {
+    user_avatar: 'string',
+    first_name: 'string',
+    last_name: 'string',
+    user_page_name: 'string',
+    use_social_links: [],
+  },
+]
+
+const edit = [
+  {
+    title: 'string',
+    description: 'string',
+    url: 'string',
+    urls: [],
+  },
+]
