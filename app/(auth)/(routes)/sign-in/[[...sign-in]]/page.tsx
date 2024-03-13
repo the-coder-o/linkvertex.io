6b4f13940c8b396844545c3bfabb2138fad9c7e1
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-03-13 13:12:50 +0500

    chnaged some styles, added loader component and fixed some bugs

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,9 @@
 import { SignIn } from '@clerk/nextjs'
-import Animation from '@/components/animation/framer-animaion'
 
 export default function Page() {
   return (
-    <Animation delay={1} className={'flex items-center justify-center h-[100vh]'}>
+    <div className={'flex items-center justify-center h-[100vh]'}>
       <SignIn afterSignInUrl={'/dashboard'} />
-    </Animation>
+    </div>
   )
 }
--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,9 @@
 import { SignUp } from '@clerk/nextjs'
-import Animation from '@/components/animation/framer-animaion'
 
 export default function Page() {
   return (
-    <Animation delay={1} className={'flex items-center justify-center h-[100vh]'}>
+    <div className={'flex items-center justify-center h-[100vh]'}>
       <SignUp afterSignUpUrl={'/dashboard'} />
-    </Animation>
+    </div>
   )
 }
