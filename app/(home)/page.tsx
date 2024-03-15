212606a58f486df15ec2b8e575a98af035a261b8
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-03-15 14:52:01 +0500

    changed loader added new loader

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -13,27 +13,27 @@ export default function Home() {
   const { isSignedIn,user } = useUser()
   const { t } = useTranslation('home_section')
 
-  if (user){
-    return (
-    <div className={'container'}>
-      <div className={'flex h-[100vh] flex-col items-center justify-center gap-[3.5rem]'}>
-        <Animation delay={0}>
-          <h1 className={'main-text-animation w-[410px] text-center text-[3.80rem] font-[800] leading-[110%] max-md:w-[508px] max-md:text-[48px] max-sm:w-[450px] max-[530px]:w-full'}>{t('title')}</h1>
-        </Animation>
-        <Animation delay={1}>
-          <p className={'ml-auto mr-auto w-[410px] text-center font-[600] leading-[1.5rem] text-white max-sm:w-[85%]'}>{t('subtitle')}</p>
-        </Animation>
-        <Animation delay={2}>
-          <Link href={isSignedIn ? '/dashboard' : '/sign-in'} className={'rounded-[20px] bg-[#FCD28D] px-[24px] py-[12px] text-[18px] font-[600] transition-all hover:bg-[#EBB064]'}>
-            {t('button')}
-          </Link>
-        </Animation>
-      </div>
-      <Footer />
-    </div>
-  )
-  }else {
+  if (!user){
     return <Loader/>
+  }else {
+    return (
+        <div className={'container'}>
+          <div className={'flex h-[100vh] flex-col items-center justify-center gap-[3.5rem]'}>
+            <Animation delay={0}>
+              <h1 className={'main-text-animation w-[410px] text-center text-[3.80rem] font-[800] leading-[110%] max-md:w-[508px] max-md:text-[48px] max-sm:w-[450px] max-[530px]:w-full'}>{t('title')}</h1>
+            </Animation>
+            <Animation delay={1}>
+              <p className={'ml-auto mr-auto w-[410px] text-center font-[600] leading-[1.5rem] text-white max-sm:w-[85%]'}>{t('subtitle')}</p>
+            </Animation>
+            <Animation delay={2}>
+              <Link href={isSignedIn ? '/dashboard' : '/sign-in'} className={'rounded-[20px] bg-[#FCD28D] px-[24px] py-[12px] text-[18px] font-[600] transition-all hover:bg-[#EBB064]'}>
+                {t('button')}
+              </Link>
+            </Animation>
+          </div>
+          <Footer />
+        </div>
+    )
   }
 
 }
--- components/loader.tsx
+++ components/loader.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import { ClipLoader } from 'react-spinners'
+import {DotBackgroundDemo} from "@/components/animation/dot-background";
 
 const Loader = () => {
   return (
     <div className={'fixed bottom-0 left-0 right-0 top-0 z-[9999999999] flex items-center justify-center bg-black'}>
-      <ClipLoader size={50} aria-label="Loading Spinner" data-testid="loader" color={'#fff'} />
+        <div className="loader">
+            <div className="loader-circle"></div>
+            <span className="text-[#7e3af2] text-[26px] font-bold">Loading...</span>
+        </div>
+        <div className={'opacity-25 fixed left-0 right-0 top-0 bottom-0'}>
+            <DotBackgroundDemo />
+        </div>
     </div>
   )
 }
