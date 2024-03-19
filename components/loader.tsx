5f9046a050b0dab46f7ef58695a64db0fdc008b2
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-03-19 19:43:35 +0500

    released new design loader for project and fixed bugs

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import {DotBackgroundDemo} from "@/components/animation/dot-background";
+import React from "react";
+import { DotBackgroundDemo } from "@/components/animation/dot-background";
 
 const Loader = () => {
   return (
-    <div className={'fixed bottom-0 left-0 right-0 top-0 z-[9999999999] flex items-center justify-center bg-black'}>
-        <div className="loader">
-            <div className="loader-circle"></div>
-            <span className="text-[#7e3af2] text-[26px] font-bold">Loading...</span>
+    <div className={"fixed bottom-0 left-0 right-0 top-0 z-[9999999999] flex items-center justify-center bg-black"}>
+      <div className="loader">
+        <div className="loader-circle"></div>
+        <div className="loading loading03 !flex !items-center !gap-2">
+          <span className={"!font-[900]"}>L</span>
+          <span className={"!font-[900]"}>O</span>
+          <span className={"!font-[900]"}>A</span>
+          <span className={"!font-[900]"}>D</span>
+          <span className={"!font-[900]"}>I</span>
+          <span className={"!font-[900]"}>N</span>
+          <span className={"!font-[900]"}>G</span>
         </div>
-        <div className={'opacity-25 fixed left-0 right-0 top-0 bottom-0'}>
-            <DotBackgroundDemo />
-        </div>
+      </div>
+      <div className={"fixed bottom-0 left-0 right-0 top-0 opacity-25"}>
+        <DotBackgroundDemo />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
