86be9b0cf99a050c6808fb5356ec4cc6660ca319
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-03-19 12:23:08 +0500

    finished, fixed bugs from database

--- lib/current-profile.ts
+++ lib/current-profile.ts
@@ -3,9 +3,10 @@
 import { auth } from "@clerk/nextjs";
 
 import { db } from "@/lib/db";
+import { User } from "@prisma/client";
 
-export const currentProfile = async () => {
-  const { userId }: any = auth();
+export const currentProfile = async (): Promise<User | null> => {
+  const { userId } = auth();
 
   if (!userId) {
     return null;
