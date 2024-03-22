9877084ec138305fed1c2f6d55b232ff78af614f
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-03-22 08:41:46 +0500

    doing users page, updated dahsboard/settings page added Position to display socials

--- app/(root)/dashboard/profile/page.tsx
+++ app/(root)/dashboard/profile/page.tsx
@@ -1,56 +1,56 @@
-'use client'
+"use client";
 
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, useState } from "react";
 
-import Link from 'next/link'
+import Link from "next/link";
 
-import { ChevronLeft } from 'lucide-react'
-import { useTranslation } from 'react-i18next'
+import { ChevronLeft } from "lucide-react";
+import { useTranslation } from "react-i18next";
 
-import { Button } from '@/components/ui/button'
-import SocialLinks from './components/SocialLinks'
-import Animation from '@/components/animation/framer-animaion'
+import { Button } from "@/components/ui/button";
+import SocialLinks from "./components/SocialLinks";
+import Animation from "@/components/animation/framer-animaion";
 
 const Profile = () => {
-  const [input, setInput] = useState({ instagram: '', mastodon: '', tiktok: '', telegram: '', pinterest: '', github: '', whatsapp: '', email: '', spotify: '', appleMusic: '', snapchat: '', appStore: '', googlePlay: '', facebook: '', youtube: '', twitch: '', linkedin: '', twitter: '' })
-  const [isValid, setIsValid] = useState({ instagram: true, mastodon: true, tiktok: true, telegram: true, pinterest: true, github: true, whatsapp: true, email: true, spotify: true, appleMusic: true, snapchat: true, appStore: true, googlePlay: true, facebook: true, youtube: true, twitch: true, linkedin: true, twitter: true })
-  const { t } = useTranslation('social_links_page')
+  const [input, setInput] = useState({ instagram: "", mastodon: "", tiktok: "", telegram: "", pinterest: "", github: "", whatsapp: "", email: "", spotify: "", appleMusic: "", snapchat: "", appStore: "", googlePlay: "", facebook: "", youtube: "", twitch: "", linkedin: "", twitter: "" });
+  const [isValid, setIsValid] = useState({ instagram: true, mastodon: true, tiktok: true, telegram: true, pinterest: true, github: true, whatsapp: true, email: true, spotify: true, appleMusic: true, snapchat: true, appStore: true, googlePlay: true, facebook: true, youtube: true, twitch: true, linkedin: true, twitter: true });
+  const { t } = useTranslation("social_links_page");
 
   const handleChange = (service: string, validator: (input: string) => boolean) => (event: ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target
-    setInput({ ...input, [service]: value })
-    setIsValid({ ...isValid, [service]: validator(value) })
-  }
+    const { value } = event.target;
+    setInput({ ...input, [service]: value });
+    setIsValid({ ...isValid, [service]: validator(value) });
+  };
 
-  const isSaveDisabled = Object.values(isValid).some((value) => !value)
+  const isSaveDisabled = Object.values(isValid).some((value) => !value);
 
   return (
     <>
       <head>
-        <title>Profile | Linkvertex</title>
+        <title>Add Icon | Linkvertex</title>
       </head>
-      <div className={'container'}>
-        <div className={'mt-[100px]'}>
+      <div className={"container"}>
+        <div className={"mt-[100px]"}>
           <Animation delay={0}>
-            <Link href={'/dashboard'} className={'flex w-[13%] items-center justify-center rounded-[24px] bg-[#1D1F1F]/80 py-[8px] pr-[10px] text-[16px] font-[500] text-white transition-all hover:bg-[#1D1F1F] max-sm:w-[100px]'}>
-              <ChevronLeft className={'h-5 w-5 font-bold'} />
-              {t('back_btn')}
+            <Link href={"/dashboard"} className={"flex w-[13%] items-center justify-center rounded-[24px] bg-[#1D1F1F]/80 py-[8px] pr-[10px] text-[16px] font-[500] text-white transition-all hover:bg-[#1D1F1F] max-sm:w-[100px]"}>
+              <ChevronLeft className={"h-5 w-5 font-bold"} />
+              {t("back_btn")}
             </Link>
           </Animation>
           <Animation delay={1}>
-            <div className={'mb-[3rem]'}>
-              <h2 className={'main-text-animation my-[16px] text-4xl font-bold max-sm:text-[30px]'}>{t('title')}</h2>
+            <div className={"mb-[3rem]"}>
+              <h2 className={"main-text-animation my-[16px] text-4xl font-bold max-sm:text-[30px]"}>{t("title")}</h2>
             </div>
           </Animation>
           <Animation delay={2}>
-            <div className={'flex flex-col gap-[2.5rem]'}>
+            <div className={"flex flex-col gap-[2.5rem]"}>
               <div>
-                <h2 className={'mb-[16px] text-3xl font-bold text-white max-sm:text-[26px]'}>{t('subtitle')}</h2>
+                <h2 className={"mb-[16px] text-3xl font-bold text-white max-sm:text-[26px]"}>{t("subtitle")}</h2>
               </div>
-              <div className={'mb-[100px] grid grid-cols-1 space-y-[2rem]'}>
+              <div className={"mb-[100px] grid grid-cols-1 space-y-[2rem]"}>
                 <SocialLinks input={input} handleChange={handleChange} isValid={isValid} />
-                <Button disabled={isSaveDisabled} className={'text-md rounded-[20px] bg-[#9ae6b4] font-semibold text-black hover:bg-[#9ae6c9]'}>
-                  {t('save_btn')}
+                <Button disabled={isSaveDisabled} className={"text-md rounded-[20px] bg-[#9ae6b4] font-semibold text-black hover:bg-[#9ae6c9]"}>
+                  {t("save_btn")}
                 </Button>
               </div>
             </div>
@@ -58,7 +58,7 @@ const Profile = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
